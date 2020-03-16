fa7a9802f659331e18e3e2eaa37d384cb57ff5f4
s18732 <s18732@pjwstk.edu.pl>
2020-03-16 17:03:41 +0100

    rozpoczecie cwiczen

---
+++ cw3/cw3/DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.Models;
+
+namespace cw3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
